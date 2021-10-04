14fcd53b9e947b2f78a7fdae39e2a9e98196057d
isaacbarcroft <i_barcroft@yahoo.com>
2021-10-03 21:21:58 -0400

    menu

--- src/menu.js
+++ src/menu.js
@@ -34,12 +34,62 @@ const menu = [
         desc: `I'm baby woke mlkshk wolf bitters',`
     },
     {
+        id: 18,
+        title: 'French Toast',
+        category: 'Breakfast',
+        price: 10.99,
+        img: '',
+        desc: ``
+    },
+    
+    {
+        id: 15,
+        title: 'Eggs Bennies',
+        category: 'Breakfast',
+        price: 10.99,
+        img: '',
+        desc: ``
+    },
+    {
+        id: 16,
+        title: 'Chicken & Waffles',
+        category: 'Breakfast',
+        price: 14.99,
+        img: '',
+        desc: ``
+    },
+    {
+        id: 17,
+        title: 'Breakfast Burrito',
+        category: 'Breakfast',
+        price: 10.99,
+        img: '',
+        desc: ``
+    },
+   
+    {
+        id: 12,
+        title: 'Wings',
+        category: 'Lunch',
+        price: 9.99,
+        img: '',
+        desc: `Sauces: Barbecue, Jerk, Franks, Sweet-Chili`
+    },
+    {
         id: 5,
-        title: 'Chicken Tendies',
+        title: 'Burger',
         category: 'Lunch',
         price: 12.99,
         img: '',
-        desc: `Fried or Grilled`
+        desc: `Nothing over medium`
+    },
+    {
+        id: 7,
+        title: 'Dogs',
+        category: 'Lunch',
+        price: 10.99,
+        img: 'src/images/dgos.jpeg',
+        desc: `Dog in a bun, relish included`
     },
     {
         id: 6,
@@ -49,13 +99,65 @@ const menu = [
         img: 'src/images/nugs.jpeg',
         desc: `I'm baby woke mlkshk wolf bitters',`
     },
+   
     {
-        id: 7,
-        title: 'Dogs',
+        id: 24,
+        title: 'Turkey BLT',
         category: 'Lunch',
-        price: 10.99,
-        img: 'src/images/dgos.jpeg',
-        desc: `Dog in a bun`
+        price: 8.99,
+        img: '',
+        desc: ``
+    },
+    
+    {
+        id: 23,
+        title: 'Pork Carnita Tacos',
+        category: 'Lunch',
+        price: 15.99,
+        img: '',
+        desc: ``
+    },
+      
+    {
+        id: 19,
+        title: 'Halibut',
+        category: 'Dinner',
+        price: 28.99,
+        img: '',
+        desc: ``
+    },
+    {
+        id: 21,
+        title: 'Shrimp Scampi',
+        category: 'Dinner',
+        price: 25.99,
+        img: '',
+        desc: ``
+    },
+    {
+        id: 22,
+        title: 'Chicken Alfredo',
+        category: 'Dinner',
+        price: 19.99,
+        img: '',
+        desc: ``
+    }, 
+     
+    {
+        id: 9,
+        title: 'Chicken Piccata',
+        category: 'Dinner',
+        price: 15.99,
+        img: '',
+        desc: `Grilled Chicken Fillet`
+    },
+    {
+        id: 20,
+        title: 'Pan Seared Duck Breast',
+        category: 'Dinner',
+        price: 27.99,
+        img: '',
+        desc: ``
     },
     {
         id: 8,
@@ -66,12 +168,12 @@ const menu = [
         desc: `14 oz`
     },
     {
-        id: 9,
-        title: 'Grilled Chicken',
+        id: 11,
+        title: 'NewYork Strip',
         category: 'Dinner',
-        price: 15.99,
-        img: '',
-        desc: `Grilled Chicken Fillet`
+        price: 30.99,
+        img: './images/Eggplant-Parmesan-Recipe-f.jpeg',
+        desc: `8 oz`
     },
     {
         id: 10,
@@ -82,21 +184,23 @@ const menu = [
         desc: `Eggplant Parm`
     },
     {
-        id: 11,
-        title: 'NewYork Strip',
+        id: 13,
+        title: 'Seared Sea Scallops',
         category: 'Dinner',
-        price: 30.99,
-        img: './images/Eggplant-Parmesan-Recipe-f.jpeg',
-        desc: `8 oz`
-    },
-    {
-        id: 12,
-        title: 'Wings',
-        category: 'Dinner',
-        price: 15.99,
+        price: 29.99,
         img: '',
-        desc: `Sauces: Barbecue, Jerk, Franks, Sweet-Chili`
+        desc: ``
+    }, 
+    {
+        id: 14,
+        title: 'Swordfish',
+        category: 'Dinner',
+        price: 26.99,
+        img: '',
+        desc: ``
     },
+
+
 ];
 
 export default menu;
