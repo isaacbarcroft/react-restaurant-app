0485dff343ed2bcdb4e653a59f328911919899c0
isaacbarcroft <i_barcroft@yahoo.com>
2021-10-29 12:00:47 -0400

    deploy

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -8,7 +8,7 @@ function MenuItem(props){
     <div class="menuItemList">
         <h3 class="itemName">{props.menuItem?.title}</h3>
         <p>{` $  ${props.menuItem?.price}`}</p>
-        <img src=  { props.menuItem?.img}  alt="" />
+        <img src={props.menuItem?.image}  alt="" ></img>
         <p>'{props.menuItem?.desc}'</p>
         <button class="btn2" type="button" onClick={() => props.addOrder(props.menuItem?.title, props.menuItem?.price)}>Add to order </button>
         
